834609a1c8d1c031d2ed158bc512b3116d2a6cbe
DiegoSouza7 <diego78523@gmail.com>
2021-04-15 20:50:07 -0300

    responsivity

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,19 +1,25 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react'
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex
       align="center"
     >
-      <Box mr="4" textAlign="right" >
-        <Text>Diego Souza</Text>
-        <Text
-          color="gray.300"
-          fontSize="small"
-        >
-          Diego785237@gmail.com
-      </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right" >
+          <Text>Diego Souza</Text>
+          <Text
+            color="gray.300"
+            fontSize="small"
+          >
+            Diego785237@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Diego Souza" src="https://github.com/DiegoSouza7.png" />
     </Flex>
   )
