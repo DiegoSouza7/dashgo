bb517eeb48b1f6105943e9bb32701244e5cb6cc6
DiegoSouza7 <diego78523@gmail.com>
2021-05-09 17:23:46 -0300

    feath: Changing pages

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -48,7 +48,7 @@ export function Pagination({
       <Stack direction="row" spacing="2">
         {currentPage > (1 + siblingsCount) && (
           <>
-            <PaginationItem number={1} />
+            <PaginationItem onPageChange={onPageChange} number={1} />
             {currentPage > (2 + siblingsCount) && (
               <Text
                 color="gray.300"
@@ -59,15 +59,14 @@ export function Pagination({
           </>
         )}
         {previousPages.length > 0 && previousPages.map(page => (
-          <PaginationItem key={page} number={page} />
+          <PaginationItem onPageChange={onPageChange} key={page} number={page} />
         ))}
-        <PaginationItem number={currentPage} isCurrent />
+        <PaginationItem onPageChange={onPageChange} number={currentPage} isCurrent />
         {nextPages.length > 0 && nextPages.map(page => (
-          <PaginationItem key={page} number={page} />
+          <PaginationItem onPageChange={onPageChange} key={page} number={page} />
         ))}
         {(currentPage + siblingsCount) < lastPage && (
           <>
-            <PaginationItem number={lastPage} />
             {(currentPage + 1 +siblingsCount) < lastPage && (
               <Text
                 color="gray.300"
@@ -75,6 +74,7 @@ export function Pagination({
                 textAlign="center"
               >...</Text>
             )}
+            <PaginationItem onPageChange={onPageChange} number={lastPage} />
           </>
         )}
       </Stack>
